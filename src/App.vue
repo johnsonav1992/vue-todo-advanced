40f2637007097640c2144e2413c500e3c16f2bb7
Alex Johnson <johnsonav1992@gmail.com>
2022-10-06 15:43:55 -0500

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue Todo List</h1>
+  <form action="submit">
+				<input
+					type="text"
+					v-model="textInput"
+					placeholder="Type a todo item"
+				/>
+				<button @click="addTodo">+</button>
+			</form>
+			<section class="section">
+				<ul>
+					<li v-for="todo in todoList" :key="todo">
+						{{ todo }} <button @click="deleteTodo(todo)">-</button>
+					</li>
+				</ul>
+			</section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  data() {
+    return {
+      todoList: [],
+      textInput: ''
+    }
+  },
+  methods: {
+    addTodo(e) {
+            e.preventDefault()
+            if (this.textInput === '') return
+            this.todoList.push(this.textInput)
+            this.textInput = ''
+        },
+        deleteTodo(todo) {
+            const todoIndex = this.todoList.indexOf(todo)
+            this.todoList.splice(todoIndex, 1)
+        }
+  },
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -19,8 +47,28 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.section {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+ul {
+  list-style: none;
+  display: flex;
+  justify-content: flex-start;
+  background-color: red;
+}
+
+li {
+  text-decoration: none;
 }
 </style>
